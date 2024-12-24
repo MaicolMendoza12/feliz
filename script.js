5f3d13427a577759b078ec14d892f6a8b47bf0e5
MaicolMendoza12 <luaymendoza591@gmail.com>
2024-12-24 13:02:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Countdown Functionality
+const countdown = () => {
+    const targetDate = new Date('December 25, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const timeLeft = targetDate - now;
+
+    if (timeLeft > 0) {
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById('days').textContent = days.toString().padStart(2, '0');
+        document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+        document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+        document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+    } else {
+        document.querySelector('.countdown').textContent = '¡Feliz Navidad!';
+        // Espera 3 segundos antes de redirigir
+        setTimeout(() => {
+            window.location.href = "12.html"; // Cambia la URL por la que deseas redirigir
+        }, 2500); // 3000 milisegundos = 3 segundos
+    }
+};
+
+// Run Countdown Every Second
+setInterval(countdown, 1000);
+countdown();
